df34b79f08c2f692a4315184c657e463acd4f523
Kacy Do <contact@kacydo.dev>
2026-05-21 03:31:30 +0700

    add vercel blob plugin. Read more: 'Payload Storage Adapters'

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -9,6 +9,7 @@ import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
 import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import { searchFields } from '@/search/fieldOverrides'
 import { beforeSyncWithSearch } from '@/search/beforeSync'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -24,6 +25,12 @@ const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
 }
 
 export const plugins: Plugin[] = [
+  vercelBlobStorage({
+    collections: {
+      media: true,
+    },
+    token: process.env.BLOB_READ_WRITE_TOKEN || '',
+  }),
   redirectsPlugin({
     collections: ['pages', 'posts'],
     overrides: {
